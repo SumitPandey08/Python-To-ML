7e79eee67ac22039c4e95a26eea65a985255eca2
SumitPandey08 <sp.sumitpandey06@gmail.com>
2024-12-27 15:25:51 +0530

    Data Type And Variable

---
+++ Variable.py
@@ -0,0 +1,25 @@
+## Variable With DataType
+#In this example, we will create a variable with a specific data type. We will create a variabl
+
+a = 5 
+print(type(a))  # integer
+
+b = "string"
+print(type(b)) # string
+
+st = "tooo";
+
+print(b + " " + st)
+
+c =  True 
+print(type(c)) # boolean
+
+boolean = (1 == 2)
+print(boolean) 
+
+flot = 0.23 
+print(type(flot)) # float
+
+complexNo = 2j 
+print(type(complexNo)) # complex
+print(complexNo*complexNo)
