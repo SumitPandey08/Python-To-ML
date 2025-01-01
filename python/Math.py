b6055a7202ab7d3e637ed6784201a4c3b851f702
SumitPandey08 <sp.sumitpandey06@gmail.com>
2025-01-01 15:35:26 +0530

    math Library in Python

---
+++ python/Math.py
@@ -0,0 +1,31 @@
+import math
+
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)
+print(y)
+
+ab = abs(-3.75)
+print(ab)
+
+power = pow(4 , 5)
+print(power)
+
+# using math library
+
+srt = math.sqrt(72)
+print(srt)
+
+
+x = math.ceil(1.4)
+y = math.floor(1.4)
+
+print(x) # returns 2
+print(y) # returns 1
+
+import math
+
+x = math.pi
+
+print(x)
