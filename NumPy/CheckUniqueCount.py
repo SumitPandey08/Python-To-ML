4e6ef6648bfccc9f77275570d41a9b251ab08ddb
SumitPandey08 <sp.sumitpandey06@gmail.com>
2025-01-03 19:22:42 +0530

    Unique and Count

---
+++ NumPy/CheckUniqueCount.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+# Check unique  and count
+
+arr1 = np.array([1, 2, 3, 2 , 4  , 4 , 1 ,4 , 7 , 9 , 6 , 7 ])
+print(np.unique(arr1))
+
+arr2 = np.array([[1, 2, 3], [4, 5, 6], [1, 2, 3], [4, 5, 6]])
+print(np.unique(arr2))
+
+print(np.unique(arr2 , axis = 0))
+
+print(np.unique(arr1 , return_index=True )) # return the index of unique values
+
+print(np.unique(arr1 , return_counts=True )) # return the count of unique values
